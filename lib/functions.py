a3f4933b1498d1c41c99d0eb7b70883d604c04e0
TheRealHetfield <pvld9afi30@veil.email>
2017-10-28 18:16:22 -0500

    Updated print functions to use sys.stdout.write for better control of output

--- lib/functions.py
+++ lib/functions.py
@@ -1,34 +1,42 @@
 #!/usr/bin/python
 
 from lib.constants import TC
+import sys
 
 
 #
 #
 #
 def print_error(error):
-	print TC.RED + TC.BOLD + "[X] " + TC.END + TC.RED + error + TC.END
+	sys.stdout.write(TC.RED + TC.BOLD + "[X] " + TC.END + TC.RED + error + TC.END)
 
 
 #
 #
 #
 def print_warning(warning):
-	print TC.YELLOW + TC.BOLD + "[!] " + TC.END + TC.YELLOW + warning + TC.END
+	sys.stdout.write(TC.YELLOW + TC.BOLD + "[!] " + TC.END + TC.YELLOW + warning + TC.END)
 
 
 #
 #
 #
 def print_success(success):
-	print TC.GREEN + TC.BOLD + "[+] " + TC.END + TC.GREEN + success + TC.END
+	sys.stdout.write(TC.GREEN + TC.BOLD + "[+] " + TC.END + TC.GREEN + success + TC.END)
 
 
 #
 #
 #
 def print_info(info):
-	print TC.BLUE + TC.BOLD + "[-] " + TC.END + TC.BLUE + info + TC.END
+	sys.stdout.write(TC.BLUE + TC.BOLD + "[-] " + TC.END + TC.BLUE + info + TC.END)
+
+
+#
+#
+#
+def print_progress(text):
+	sys.stdout.write(TC.BLUE + text + TC.END)
 
 
 #
